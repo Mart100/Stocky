b17ede37cf861d6362bac965117a93886c2546ce
Marto_0 <martvanenck1@gmail.com>
2019-03-29 02:25:05 +0100

    round balance

--- commands/balance.js
+++ commands/balance.js
@@ -3,6 +3,6 @@ const database = require('../scripts/database.js')
 
 module.exports = async (message) => {
   let userInfo = await database.getUser(message.author.id)
-  message.channel.send('<@'+message.author.id+'>, you currently have '+userInfo.balance+'$ in your pocket!')
+  message.channel.send('<@'+message.author.id+'>, you currently have '+Math.round(userInfo.balance)+'$ in your pocket!')
   
 }
